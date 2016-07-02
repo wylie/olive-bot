f571c39fcfb132a275371ee825ab38af997ac9b9
Wylie Fisher <wylie@dukeofcheese.com>
2016-07-01 22:28:23 -0400

    a new script

---
+++ scripts/my-scripts.js
@@ -0,0 +1,7 @@
+module.exports = (robot) ->
+  robot.respond /open the (.*) doors/i, (res) ->
+    doorType = res.match[1]
+    if doorType is "pod bay"
+      res.reply "I'm afraid I can't let you do that."
+    else
+      res.reply "Opening #{doorType} doors"
